35eb82468e701579f241242e4616c8ff1d09034b
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-05-26 22:54:34 +0530

    REST Api completed

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,71 +1,102 @@
-const asyncHandler = require('express-async-handler')
-const Goal = require('../model/goalModel')
+const asyncHandler = require("express-async-handler");
+const Goal = require("../model/goalModel");
+const User = require("../model/userModel");
 
-//@des    Get Goals 
+//@des    Get Goals
 // @route  GET /api/goals
 // @access  Private
 
-const getGoals = asyncHandler(async (req,res)=>{
-    const goals = await Goal.find()
+const getGoals = asyncHandler(async (req, res) => {
+  const goals = await Goal.find({ user: req.user.id });
 
-    res.status(200).json(goals)
-})
+  res.status(200).json(goals);
+});
 
-//@des    Set Goals 
+//@des    Set Goals
 // @route  POST/api/goals
 // @access  Private
 
-const setGoals = asyncHandler(async (req,res)=>{
-    if(!req.body.text){
-        res.status(400)
-        throw new Error("Please add a text field");
-    }
+const setGoals = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
 
-    const goal = await Goal.create({
-        text: req.body.text
-    })
+  const goal = await Goal.create({
+    text: req.body.text,
+    user: req.user.id,
+  });
 
-
-    res.status(200).json(goal)
-})
+  res.status(200).json(goal);
+});
 
 //@des    Update Goal
 // @route  PUT /api/goals/id
 // @access  Private
 
-const updateGoals = asyncHandler(async (req,res)=>{
-    const goal = await Goal.findById(req.params.id)
+const updateGoals = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
 
-    if(!goal){
-        res.status(400)
-        throw new Error("Goal Not Found")
-    }
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal Not Found");
+  }
 
-    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  const user = await User.findById(req.user.id);
 
-    res.status(200).json(goal);
-})
+  //check for user
+  if (!user) {
+    res.status(401)
+    throw new Error("User not Found");
+  }
+
+  //make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id){
+    res.status(401)
+    throw new Error("User not authorized");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
+});
 
 //@des    Delete Goal
 // @route  GET /api/goals/id
 // @access  Private
 
-const deleteGoals = asyncHandler(async (req,res)=>{
-    const goal = await Goal.findById(req.params.id);
+const deleteGoals = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
 
-    if(!goal){
-        res.status(400)
-        throw new Error("Goal Not Found")
-    }
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal Not Found");
+  }
 
-    await goal.remove()
+  const user = await User.findById(req.user.id);
 
-    res.status(200).json({id: req.params.id});
-})
+  //check for user
+  if (!user) {
+    res.status(401)
+    throw new Error("User not Found");
+  }
+
+  //make sure the logged in user matches the goal user
+  if(goal.user.toString() !== user.id){
+    res.status(401)
+    throw new Error("User not authorized");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
-    getGoals,
-    setGoals,
-    updateGoals,
-    deleteGoals
-}
+  getGoals,
+  setGoals,
+  updateGoals,
+  deleteGoals,
+};
--- backend/routes/goRoutes.js
+++ backend/routes/goRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController');
 const router = express.Router();
+const {protect} = require('../middleware/authMiddleware')
 
-router.route("/").get(getGoals).post(setGoals);
+router.route("/").get(protect, getGoals).post(protect, setGoals);
 
-router.route("/:id").put(updateGoals).delete(deleteGoals);
+router.route("/:id").put(protect, updateGoals).delete(protect, deleteGoals);
 
 
 module.exports = router;
