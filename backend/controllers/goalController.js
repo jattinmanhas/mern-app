6d2f6841c0f0c217cb49ffa6db2cc0f891dcfeea
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-05-26 20:52:25 +0530

    Initial REST API

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,14 @@
 const asyncHandler = require('express-async-handler')
+const Goal = require('../model/goalModel')
 
 //@des    Get Goals 
 // @route  GET /api/goals
 // @access  Private
 
 const getGoals = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: "Get Goals"})
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 //@des    Set Goals 
@@ -18,8 +21,12 @@ const setGoals = asyncHandler(async (req,res)=>{
         throw new Error("Please add a text field");
     }
 
+    const goal = await Goal.create({
+        text: req.body.text
+    })
 
-    res.status(200).json({message: "Set Goals"})
+
+    res.status(200).json(goal)
 })
 
 //@des    Update Goal
@@ -27,7 +34,16 @@ const setGoals = asyncHandler(async (req,res)=>{
 // @access  Private
 
 const updateGoals = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Update Goals ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal Not Found")
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
+
+    res.status(200).json(goal);
 })
 
 //@des    Delete Goal
@@ -35,7 +51,16 @@ const updateGoals = asyncHandler(async (req,res)=>{
 // @access  Private
 
 const deleteGoals = asyncHandler(async (req,res)=>{
-    res.status(200).json({message: `Delete Goal ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400)
+        throw new Error("Goal Not Found")
+    }
+
+    await goal.remove()
+
+    res.status(200).json({id: req.params.id});
 })
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT ||  5000;
+const connectDb = require('./config/db')
+
+connectDb()
 
 const app = express();
 
