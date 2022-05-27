429db1b1dc832a6e3e0e56fcf24e8a921fcbd184
Jattin Manhas <jattinmanhas0102@gmail.com>
2022-05-27 11:31:13 +0530

    Fixed Bugs

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from "../components/GoalForm"
 import GoalItem from '../components/GoalItem'
-import GoalForm from '../components/GoalFrom'
 import Spinner from '../components/Spinner'
 import { getGoals, reset } from '../features/goals/goalSlice'
 
@@ -14,6 +14,22 @@ function Dashboard() {
   const { goals, isLoading, isError, message } = useSelector(
     (state) => state.goals
   )
+
+  // useEffect(() => {
+  //   if (isError) {
+  //     console.log(message)
+  //   }
+
+  //   if (!user) {
+  //     navigate('/login')
+  //   }
+
+  //   dispatch(getGoals())
+
+  //   return () => {
+  //     dispatch(reset())
+  //   }
+  // }, [user, navigate, isError, message, dispatch])
 
   useEffect(() => {
     if (isError) {
@@ -29,7 +45,7 @@ function Dashboard() {
     return () => {
       dispatch(reset())
     }
-  }, [user, navigate, isError, message, dispatch])
+  }, [user, dispatch, navigate])
 
   if (isLoading) {
     return <Spinner />
@@ -42,7 +58,7 @@ function Dashboard() {
         <p>Goals Dashboard</p>
       </section>
 
-      <GoalForm />
+      <GoalForm /> 
 
       <section className='content'>
         {goals.length > 0 ? (
